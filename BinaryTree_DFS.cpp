461a6f297ee05870eced11cc33b54c983d073d2f
jiho lee <jh001007@gmail.com>
2019-02-01 16:55:30 +0900

    Abstraction & Dynamic allocation

--- BinaryTree_DFS.cpp
+++ BinaryTree_DFS.cpp
@@ -1,49 +1,59 @@
 #include <iostream>
 #include <vector>
-
+ 
 using namespace std;
-
+ 
 struct binary_tree {
    int n;
    binary_tree* left;
    binary_tree* right;
 };
-
-
-int main() {
-    // 귀찮아서 배열에 박음
-    binary_tree tree[6];
-    for(int i=0;i<6;i++) {
-        tree[i].n = i;
-        tree[i].left = NULL;
-        tree[i].right = NULL;
-    }
-    tree[0].left =  &tree[1];
-    tree[0].right = &tree[2];
-    tree[2].left = &tree[3];
-    tree[2].right = &tree[4];
-    tree[3].left = &tree[5];
-    binary_tree testtree[6];
-    for(int i=0;i<6;i++) testtree[i] = tree[i];
-    vector<binary_tree> vertical = {testtree[0]};
-    while(1) {
-        if(vertical.size() == 0) break;
-        binary_tree current = vertical[vertical.size()-1];
+ 
+binary_tree* init(int n) {
+	binary_tree* tree = (binary_tree*)malloc(sizeof(binary_tree));
+	tree->n = n;
+	return tree;
+}
+ 
+binary_tree* left(binary_tree* parent, binary_tree* child) {
+	parent->left = child;
+	return child;
+}
+ 
+binary_tree* right(binary_tree* parent, binary_tree* child) {
+	parent->right = child;
+	return child;
+}
+ 
+void find(binary_tree* tree) {
+	vector<binary_tree*> vertical = {tree};
+    while(vertical.size() != 0) {
+        binary_tree* current = vertical[vertical.size()-1];
         vertical.pop_back();
-        cout << current.n << endl;
-        if(current.left == NULL && current.right == NULL && vertical.size() > 0) {
-            if(vertical[vertical.size()-1].left != NULL)vertical[vertical.size()-1].left = NULL;
-            else if(vertical[vertical.size()-1].right != NULL)vertical[vertical.size()-1].right = NULL;
+        cout << current->n << endl;
+        if(current->left == NULL && current->right == NULL && vertical.size() > 0) {
+            if(vertical[vertical.size()-1]->left != NULL) vertical[vertical.size()-1]->left = NULL;
+            else if(vertical[vertical.size()-1]->right != NULL) vertical[vertical.size()-1]->right = NULL;
             else break;
         }
-        else if(current.left != NULL) {
-vertical.push_back(current);
-vertical.push_back(*current.left);
-}
-        else if(current.right != NULL) {
-vertical.push_back(current);
-vertical.push_back(*current.right);
-}
+        else if(current->left != NULL) {
+			vertical.push_back(current);
+			vertical.push_back(current->left);
+		}
+        else if(current->right != NULL) {
+			vertical.push_back(current);
+			vertical.push_back(current->right);
+		}
     }
+    return;
+}
+ 
+int main() {
+    binary_tree* first = init(0);
+    binary_tree* current = first;
+    for(int i=1;i<11;i++) current = left(current, init(i));
+    current = first;
+    for(int i=11;i<21;i++) current = right(current, init(i));
+    find(first);
     return 0;
 }
